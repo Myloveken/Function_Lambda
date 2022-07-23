373558ef1049365ac0cfe439a6cf274a8d2a7104
Kenlove Chereste <cherestek@gmail.com>
2022-07-23 00:16:38 -0300

    aula.80

--- src/Lambda02.java
+++ src/Lambda02.java
@@ -1,10 +1,9 @@
-ackage lambdas;
 import java.util.ArrayList;
 
 public class Lambda02 {
 
     public static void main(String[] args) {
-    
+
         ArrayList<Integer> numbers = new ArrayList<Integer>();
 
         numbers.add(45);
@@ -21,12 +20,11 @@ public class Lambda02 {
         System.out.println();
 
         numbers.forEach(
-            n -> {
-                if (n % 2 == 0){ 
-                    System.out.println(n);
-                }
-            }
-        );
+                n -> {
+                    if (n % 2 == 0) {
+                        System.out.println(n);
+                    }
+                });
 
     }
 
